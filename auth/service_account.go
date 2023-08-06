087dc0df4a406feb004171acfc5ccc3397c22cea
agaddis02 <agaddis02@live.com>
2023-08-05 19:49:53 -0500

    add service account for auth

---
+++ auth/service_account.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type ServiceAccount struct {
+	ClientEmail  string `json:"client_email"`
+	ClientID     string `json:"client_id"`
+	PrivateKey   string `json:"private_key"`
+	PrivateKeyID string `json:"private_key_id"`
+}
+
+func FromFile(path string) (ServiceAccount, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return ServiceAccount{}, err
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return ServiceAccount{}, err
+	}
+
+	var sa ServiceAccount
+	err = json.Unmarshal(bytes, &sa)
+	if err != nil {
+		return ServiceAccount{}, err
+	}
+
+	return sa, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Arch-Energy-Partners/combocurve-api-go
+
+go 1.20
